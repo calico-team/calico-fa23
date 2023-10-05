3508f0259a9e7887655cf3b979f5bab4b33f312d
Sam Poder <sam.r.poder@gmail.com>
2023-10-05 14:46:12 -0700

    add the basics of rotate

---
+++ rotate/templates/rotate_template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Return the location of the K-th element in 
+ * an array of N-length after shuffling.
+ * 
+ * N: a non-negative integer
+ * K: another non-negative integer
+ */
+int solve(int N, int K) {
+    // YOUR CODE HERE
+    return 0;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    int K;
+    cin >> K;
+    cout << solve(N, K) << '\n';
+}
---
+++ rotate/templates/rotate_template.py
@@ -0,0 +1,19 @@
+def solve(N: int, K: int) -> int:
+    """
+    Return the location of the K-th element in 
+    an array of N-length after shuffling.
+    
+    N: a non-negative integer
+    K: another non-negative integer
+    """
+    # YOUR CODE HERE
+    return 0
+
+
+def main():
+    N = int(input())
+    K = int(input())
+    print(solve(N, K))
+
+if __name__ == '__main__':
+    main()
