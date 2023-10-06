e480b0bccdab7885af19aae5ea590b2ace62db2c
Ryan Zhu <ryanzhu@berkeley.edu>
2023-10-06 15:22:42 -0700

    Relocate Sam's changes lmao

---
+++ rotate/templates/rotate_template.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class Solution {
+    /**
+     * Return the location of the K-th element in 
+     * an array of N-length after shuffling.
+     * 
+     * N: a non-negative integer
+     * K: another non-negative integer
+     */
+    static int solve(int N, int K) {
+        // YOUR CODE HERE
+        return 0;
+    }
+    
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(in.readLine());
+        int K = Integer.parseInt(in.readLine());
+        out.println(solve(N, K));
+        out.flush();
+    }
+}
