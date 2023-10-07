cd86c15a3f81842766ac5af566003fec34cab335
Ryan Zhu <ryanzhu@berkeley.edu>
2023-10-07 01:51:16 -0700

    Adjust python solution spacing

--- rotate/submissions/accepted/rotate_queue.py
+++ rotate/submissions/accepted/rotate_queue.py
@@ -4,9 +4,11 @@ from collections import deque
 def solve(N: int, K: int) -> int:
     shuffled = []
     unshuffled = deque(range(1, N + 1))
+    
     while unshuffled:
         unshuffled.append(unshuffled.popleft())
         shuffled.append(unshuffled.popleft())
+    
     return shuffled.index(K) + 1
 
 
--- rotate/submissions/accepted/rotate_simulation.py
+++ rotate/submissions/accepted/rotate_simulation.py
@@ -1,8 +1,10 @@
 def solve(N: int, K: int) -> int:
     deck = list(range(1, N + 1))
+    
     for i in range(N):
         card = deck.pop(i)
         deck.append(card)
+    
     return deck.index(K) + 1
 
 
