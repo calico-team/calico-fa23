68ffd78bb78782663ed86452de4cf30d875ce7d6
Ryan Zhu <ryanzhu@berkeley.edu>
2023-10-07 02:15:01 -0700

    Delete files copied from Discord

--- rotate/experiments/rotate_queue.py
+++
@@ -1,9 +0,0 @@
-from collections import deque
-
-def solve(N, K):
-    unshuffled = deque(range(1, N + 1))
-    shuffled = []
-    while unshuffled:
-        unshuffled.append(unshuffled.popleft())
-        shuffled.append(unshuffled.popleft())
-    return shuffled.index(K) + 1
