0291007e2e9bac5d30d454a29235848e25ac876b
Ryan Zhu <ryanzhu@berkeley.edu>
2023-10-07 01:52:41 -0700

    Change C++ template to use int instead of ll

--- rotate/templates/rotate_template.cpp
+++ rotate/templates/rotate_template.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-typedef long long ll;
-
 /**
  * Return the position of the card labelled K after shuffling a deck with N
  * cards.
@@ -11,14 +9,14 @@ typedef long long ll;
  * N: the number of cards in the deck
  * K: the label of the target card
  */
-ll solve(ll N, ll K) {
+int solve(int N, int K) {
     // YOUR CODE HERE
     return 0;
 }
 
 int main() {
     int T;
-    ll N, K;
+    int N, K;
     cin >> T;
     for (int i = 0; i < T; i++) {
         cin >> N >> K;
