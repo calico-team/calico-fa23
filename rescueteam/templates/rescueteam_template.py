cd348513cda2184941d073c396760c22f8d569ca
Ryan Zhu <ryanzhu@berkeley.edu>
2023-10-14 04:07:24 -0700

    Clean up python template

--- rescueteam/templates/rescueteam_template.py
+++ rescueteam/templates/rescueteam_template.py
@@ -16,29 +16,23 @@ def solve(F: int, B: int, N: int, M: int, S: int, E: int, X: list[int], Y: list[
     '''
     # YOUR CODE HERE
 
+
 def main():
     T = int(input())
     for _ in range(T):
-        info = input().strip().split(' ')
-        F, B = int(info[0]), int(info[1])
+        F, B = map(int, input().split())
+        N, M, S, E = map(int, input().split())
 
-        info = input().strip().split(' ')
-        N, M, S, E = int(info[0]), int(info[1]), int(info[2]), int(info[3])
-
-        X = [None for _ in range(M)]
-        Y = [None for _ in range(M)]
-        U = [None for _ in range(F)]
-        V = [None for _ in range(F)]
-
+        X, Y = [None] * M, [None] * M
         for i in range(M):
-            info = input().strip().split(' ')
-            X[i], Y[i] = int(info[0]), int(info[1])
-        
+            X[i], Y[i] = map(int, input().split())
+
+        U, V = [None] * F, [None] * F
         for i in range(F):
-            info = input().strip().split(' ')
-            U[i], V[i] = int(info[0]), int(info[1])
+            U[i], V[i] = map(int, input().split())
         
-        solve(F, B, N, M, S, E, X, Y, U, V);
+        print(solve(F, B, N, M, S, E, X, Y, U, V))
+
 
 if __name__ == '__main__':
     main()
