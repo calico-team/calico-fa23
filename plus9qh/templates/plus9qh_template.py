d88eeba506f65a476c1c0d3f013b8fcf69d73bcc
Ryan Zhu <ryanzhu@berkeley.edu>
2023-10-14 01:26:13 -0700

    Clean up python template

--- plus9qh/templates/plus9qh_template.py
+++ plus9qh/templates/plus9qh_template.py
@@ -1,10 +1,9 @@
-def solve(N: int, M: int, X: list[int], Y: list[int], Z: list[int]):
+def solve(N: int, X: list[str]):
     """
-    Output a program that outputs the given text.
+    Find an HQ9+ program that exactly outputs the given text.
 
-    T: the number of Test Cases
-    N: the number of lines in the program
-    X: the list of lines of the program
+    N: the number of lines in the text
+    X: a list containing the lines of the text
     """
     # YOUR CODE HERE
     return
@@ -13,12 +12,8 @@ def solve(N: int, M: int, X: list[int], Y: list[int], Z: list[int]):
 def main():
     T = int(input())
     for _ in range(T):
-        info = input().strip().split(' ')
-        N = int(info[0])
-        X  = []
-        for i in range(N):
-            info = input().strip().split(' ')
-            X.append(int(info[0]))
+        N = int(input())
+        X = [input() for _ in range(N)]
         solve(N, X)
 
 
