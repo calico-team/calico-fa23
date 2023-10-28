7a7eb197432cb9508d9c2b4a96ec7ab4422c91c7
Zaid Maayah <49039050+zaidmaayah@users.noreply.github.com>
2023-10-28 16:30:44 -0700

    setup

---
+++ subway/templates/add_template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Return the sum of A and B.
+ * 
+ * A: a non-negative integer
+ * B: another non-negative integer
+ */
+int solve(int A, int B) {
+    // YOUR CODE HERE
+    return -1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int A, B;
+        cin >> A >> B;
+        cout << solve(A, B) << '\n';
+    }
+}
