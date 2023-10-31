ef3c185a4bcc6cce0d6eb8bd382af9e4a8c293a8
git-doge <skywire2000@gmail.com>
2023-10-31 14:47:09 -0700

    initialize interactive script

---
+++ wtds/executables/run/run.py
@@ -0,0 +1,181 @@
+# yes, i'm aware this doesn't accurately simulate the 50/50 and 4* pity systems
+# faithfully to the actual mechanics. i'm too lazy to implement it all and it's
+# not gonna affect the problem anyway.
+
+import random
+
+def run():
+    log(f'Begin interaction')
+    
+    T = int(input_test_in())
+    print_prog(T)
+    
+    N, K, X = map(int, input_test_in().split())
+    
+    total_queries = 0
+    
+    for case in range(1, T + 1):
+        log(f'Begin test case #{case} of {T}')
+        
+        P, seed = map(int, input_test_in().split())
+        random.seed(seed)
+        
+        deck = [i for i in range(1, N + 1) if i != P]
+        
+        while True:
+            type, arg = parse_query(input_prog())
+            if type == 'draw':
+                draw(deck, K)
+            elif type == 'check':
+                if not arg.isdigit():
+                    give_WA(f'Invalid argument for check: "{arg}"')
+                arg = int(arg)
+                if not (1 <= arg <= N):
+                    give_WA(f'Check argument out of range: "{arg}"')
+                check(arg, P)
+            else:
+                give_WA(f'Invalid query format: "{type}"')
+            
+            total_queries += 1
+            
+            if arg == P:
+                break
+    
+    if total_queries > X * 500:
+        give_WA(f'Too many queries. Average should be below {X} but was {total_queries / 500}.')
+    else:
+        print_prog('CORRECT')
+    
+    log('End interaction')
+    give_AC()
+
+def parse_query(query_str):
+    query_str = query_str.strip()
+    if query_str.startswith('check'):
+        s = query_str.split()
+        if s[0] == 'check' and len(s) == 2:
+            return 'check', s[1]
+    return query_str, None
+
+def draw(deck, K):
+    print_prog(*map(str, random.sample(deck, k=K)))
+
+def check(arg, P):
+    if arg == P:
+        print_prog('ABSENT')
+    else:
+        print_prog('PRESENT')
+
+################################################################################
+
+import io
+import sys
+
+_log = []
+_test_in, _prog_out = None, None
+
+def main():
+    if len(sys.argv) != 3:
+        _print_err('Incorrect number of arguments')
+        exit(1)
+    
+    _log_without_tabs('Judge:\tUser:\tInfo:')
+    
+    _, test_in_path, prog_out_path = sys.argv
+    with open(test_in_path, 'r') as test_in:
+        with open(prog_out_path, 'w') as prog_out:
+            global _test_in, _prog_out
+            _test_in, _prog_out = test_in, prog_out
+            run()
+
+def give_AC():
+    try:
+        temp = ''
+        while not temp:
+            temp = input().strip()
+        give_WA(f'Unexpected trailing output when judge was finished: {temp}')
+    except EOFError:
+        pass
+    
+    _print_prog_out('AC')
+    exit(0)
+
+def give_WA(reason):
+    log('End of log')
+    
+    _print_prog_out('WA')
+    _print_prog_out('Reason:', reason)
+    _print_prog_out('Interaction Log:')
+    for line in _log:
+        _print_prog_out(line)
+    
+    try:
+        print('WRONG_ANSWER', flush=True)
+    except BrokenPipeError as e:
+        pass
+    
+    _exhaust_input_prog()
+    exit(0)
+
+def give_IE(reason):
+    log('End of log')
+    
+    _print_err('Run executable internal error:', reason)
+    
+    _print_prog_out('IE')
+    _print_prog_out('Run executable internal error:', reason)
+    _print_prog_out('Interaction Log:')
+    for line in _log:
+        _print_prog_out(line)
+    
+    _exhaust_input_prog()
+    exit(1)
+
+def input_test_in():
+    try:
+        return _test_in.readline().strip()
+    except EOFError:
+        give_IE('End of test input while judge expected more input')
+
+def input_prog():
+    try:
+        prog_output = input()
+        _log_without_tabs(f'\t{prog_output}')
+        return prog_output
+    except (EOFError, BrokenPipeError): # TODO check if BrokenPipeError is necessary
+        give_WA('User program exited early when judge expected more output')
+
+def _exhaust_input_prog():
+    try:
+        while True:
+            input()
+    except:
+        pass
+
+def print_prog(*args, **kwargs):
+    prog_input = io.StringIO()
+    print(*args, **kwargs, file=prog_input, end='')
+    try:
+        print(*args, **kwargs, flush=True)
+        _log_without_tabs(prog_input.getvalue())
+    except BrokenPipeError:
+        temp = prog_input.getvalue()
+        give_WA(f'User program exited early when judge had more output: {temp}')
+
+def _print_prog_out(*args, **kwargs):
+    print(*args, **kwargs, file=_prog_out)
+
+def _print_err(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
+def log(message):
+    _log_without_tabs(f'\t\t{message}')
+
+def _log_without_tabs(message):
+    if len(message) > 100:
+        message = message[:100] + ' (truncated after 50 bytes)'
+    _log.append(message)
+
+if __name__ == '__main__':
+    main()
+
