be3154f0a2a6dc6c94ebfde0262acffe7f578fd4
rohan19a <arohan19@gmail.com>
2023-11-07 14:44:16 -0800

    testcases

--- example/make_data.py
+++ example/make_data.py
@@ -19,7 +19,7 @@ from calico_lib import make_sample_test, make_secret_test, make_data
 Seed for the random number generator. We need this so randomized tests will
 generate the same thing every time. Seeds can be integers or strings.
 """
-SEED = 'TODO Change this to something different, long, and arbitrary.'
+SEED = 'TODO Change this to something different, long, and arbndfjkasnfadnfoandfaifnbaewjhfbakejfbawfbeibafitrary.'
 
 
 class TestCase:
@@ -31,9 +31,8 @@ class TestCase:
     """
 
 
-    def __init__(self, A, B):
+    def __init__(self, A):
         self.A = A
-        self.B = B
 
 
 def make_sample_tests():
@@ -49,17 +48,14 @@ def make_sample_tests():
     identify edge cases.
     """
     main_sample_cases = [
-        TestCase(7, 9),
-        TestCase(420, 69),
-        TestCase(3, 0),
+        TestCase(4),
+        TestCase(20),
+        TestCase(23),
+        TestCase(321),
+        TestCase(1),
     ]
     make_sample_test(main_sample_cases, 'main')
-    
-    bonus_sample_cases = [
-        TestCase(123456789, 987654321),
-        TestCase(3141592653589793238462643, 3832795028841971693993751),
-    ]
-    make_sample_test(bonus_sample_cases, 'bonus')
+
 
 
 def make_secret_tests():
@@ -77,28 +73,21 @@ def make_secret_tests():
         def random_n_digit_number(n):
             return random.randint(10 ** (n - 1), (10 ** n) - 1) if n != 0 else 0
         A_digits = random.randint(0, max_digits)
-        B_digits = random.randint(0, max_digits)
-        A, B = random_n_digit_number(A_digits), random_n_digit_number(B_digits)
-        return TestCase(A, B)
+        A = random_n_digit_number(A_digits)
+        return TestCase(A)
     
     main_edge_cases = [
-        TestCase(0, 0),
-        TestCase(1, 0),
-        TestCase(0, 1),
-        TestCase(10 ** 9, 0),
-        TestCase(0, 10 ** 9),
-        TestCase(10 ** 9, 10 ** 9),
+        TestCase(1),
+        TestCase(9999),
     ]
     make_secret_test(main_edge_cases, 'main_edge')
     
     for i in range(5):
-        main_random_cases = [make_random_case(9) for _ in range(100)]
+        main_random_cases = [make_random_case(8) for _ in range(100)]
         make_secret_test(main_random_cases, 'main_random')
     
     bonus_edge_cases = [
-        TestCase(10 ** 100, 0),
-        TestCase(0, 10 ** 100),
-        TestCase(10 ** 100, 10 ** 100),
+        TestCase(3 * 0 ** 6),
     ]
     make_secret_test(bonus_edge_cases, 'bonus_edge')
     
@@ -117,7 +106,7 @@ def make_test_in(cases, file):
     T = len(cases)
     print(T, file=file)
     for case in cases:
-        print(f'{case.A} {case.B}', file=file)
+        print(f'{case.A}', file=file)
 
 
 def make_test_out(cases, file):
@@ -132,7 +121,7 @@ def make_test_out(cases, file):
     """
     from submissions.accepted.add_arbitrary import solve
     for case in cases:
-        print(solve(case.A, case.B), file=file)
+        print(solve(case.A), file=file)
 
 
 def main():
