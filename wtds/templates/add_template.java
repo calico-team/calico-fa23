f3641ca70c8e89998276ab31482f8ce202118fcc
Jia Jun Yu <DryStuffz>
2023-11-09 22:05:59 -0800

    added templates

--- wtds/templates/add_template.java
+++ wtds/templates/add_template.java
@@ -1,27 +1,62 @@
 import java.io.*;
 
 class Solution {
-    /**
-     * Return the sum of A and B.
-     * 
-     * A: a non-negative integer
-     * B: another non-negative integer
-     */
-    static int solve(int A, int B) {
-        // YOUR CODE HERE
-        return -1;
-    }
-    
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
+    /*
+    * Find the unknown datamon through feed, poop, and guess queries.
+        
+    * Call the feed, poop, and guess functions below to make feed, poop, and guess queries.
+    */
+     static void solve() throws IOException {
+         // YOUR CODE HERE
+     }
 
-    public static void main(String[] args) throws IOException {
-        int T = Integer.parseInt(in.readLine());
-        for (int i = 0; i < T; i++) {
-            String[] temp = in.readLine().split(" ");
-            int A = Integer.parseInt(temp[0]), B = Integer.parseInt(temp[1]);
-            out.println(solve(A, B));
-        }
+    /*
+    * Feed the datamon an integer
+    */
+     static String feed(int i) throws IOException {
+        out.println("feed " + i);
         out.flush();
-    }
-}
+         String response = in.readLine();
+         if (response.equals("WRONG_ANSWER")) {
+             System.exit(0);
+         }
+         return response;
+     }
+
+    /*
+    * Datamon poops out an integer depending on its species 
+    */
+     static int poop() throws IOException {
+        out.println("poop");
+        out.flush();
+        String response = in.readLine();
+         if (response.equals("WRONG_ANSWER")) {
+             System.exit(0);
+         }
+         return Integer.valueOf(response);
+     }
+
+    /*
+    * Guess the datamon 
+    */
+     static String guess(String i) throws IOException {
+         out.println("guess " + i);
+         out.flush();
+         String response = in.readLine();
+         if (response.equals("WRONG_ANSWER")) {
+             System.exit(0);
+         }
+         return response;
+     }
+     
+     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+     static PrintWriter out = new PrintWriter(System.out);
+ 
+     public static void main(String[] args) throws IOException {
+         int T = Integer.parseInt(in.readLine());
+         for (int i = 0; i < T; i++) {
+             solve();
+         }
+         String response = in.readLine();
+     }
+ }
