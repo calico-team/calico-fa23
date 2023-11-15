b2f9e6242723304ea75c5fee8ff062e74d166440
Zaid Maayah <49039050+zaidmaayah@users.noreply.github.com>
2023-11-14 22:24:27 -0800

    Create subway_ignore.py

---
+++ subway/submissions/accepted/subway_ignore.py
@@ -0,0 +1,32 @@
+def solve(N: int, M: int, K: int, S: list[int], E: list[int]) -> int:
+    stations = [[] for _ in range(M + 1)]
+    for s, e in zip(S, E):
+        stations[s].append(e)
+    
+    passengers_done, total_dist, curr_station = 0, 0, 1
+    train = []
+    while passengers_done < N:
+        while curr_station in train:
+            train.remove(curr_station)
+            passengers_done += 1
+        
+        while len(stations[curr_station]) > 0 and len(train) < K:
+            train.append(stations[curr_station].pop(0))
+        
+        curr_station = curr_station % M + 1
+        total_dist += 1
+    
+    return total_dist - 1
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        N, M, K = [int(x) for x in input().split()]
+        S = [int(x) for x in input().split()]
+        E = [int(x) for x in input().split()]
+        print(solve(N, M, K, S, E))
+
+
+if __name__ == '__main__':
+    main()
