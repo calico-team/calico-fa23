6fbb67cf91709fbd50134d8c4f4e575dda5c51ec
Ryan Zhu <ryanzhu@berkeley.edu>
2023-11-15 21:49:18 -0800

    Delete docstring from python solution

--- crosstown/submissions/accepted/crosstown.py
+++ crosstown/submissions/accepted/crosstown.py
@@ -1,13 +1,4 @@
 def solve(N: int, M: int, S: list[int], E: list[int]) -> int:
-    """
-    Find the longest distance travelled by any passenger when getting from their
-    starting station to their ending station.
-
-    N: the number of passengers
-    M: the number of stations
-    S: list of starting stations for each passenger
-    E: list of ending stations for each passenger
-    """
     longest = 0
     for i in range(N):
         longest = max(longest, (E[i] - S[i] + M) % M)
